2ce15c162b716fb91a2fa771a0042dd1dfd4594d
jarvychavez <jav.chavez94@gmail.com>
2025-05-04 18:43:57 -0600

    Creamos nuestra consulta pelicula

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,9 @@
 public class Principal {
     public static void main(String[] args) {
         //javier chavez
+        ConsultaPelicula consulta = new ConsultaPelicula();
+        Pelicula pelicula = consulta.buscarPelicula( 9);
+        System.out.println(pelicula);
 
     }
 }
